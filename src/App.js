a8f45721b480bdc0152a2ef975f6253f7afb4eb0
Oleksandr Yurchenko <yurchenko@trafficterminal.com>
2019-07-04 16:46:31 +0300

    json http request test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, {Component} from "react";
 import "./App.css";
 import Posts from "./components/posts";
-import PostsData from "./data/posts.json";
+// import PostsData from "./data/posts.json";
 import Filter from "./components/filter";
 
 class App extends Component {
-  state = {  
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
   }
+
+  componentDidMount() {
+    let url = "http://localhost:3001/posts"
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        let posts = data.map((post, index) => {
+          return (
+            <div key={index}>
+              <h3>{post.title}</h3>
+              <p>{post.info}</p>
+            </div>
+          )
+        })
+        this.setState({posts: posts});
+      })
+  }
+
   render() { 
     return (  
       <React.Fragment>
         <Filter/>
         <main className="container">
-          <Posts
+          {this.state.posts}
+          {/* <Posts
             posts={PostsData}
-          />
+          /> */}
         </main>
       </React.Fragment>
     );
