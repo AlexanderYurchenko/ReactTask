409557381ea41d9cf29dd619b86f9ba765608143
Alexander Yurchenko <oleksandr.yurchenko@together.com>
2019-09-29 19:27:08 +0300

    post to func component

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Link } from "react-router-dom";
 import Tags from "../tags/tags";
 import PostSingle from "../post-single/post-single";
 import "./post.scss"
 
-class Post extends Component {
-  render() {
-    const { id, title, info, tags } = this.props.post;
+const Post = ( props ) => {
+  const { id, title, info, tags } = props.post;
 
-    return (
-      <div className="c-post">
-        <div className="c-post__title-box">
-          <Link to={`/post/${id}`} className="c-post__title">{title}</Link>
-        </div>
-        <div className="c-post__info">{info}</div>
-        <div className="c-post__tags">
-          <Tags tags={tags} />
-        </div>
-        <Route path="/post/:postId"  children={(props) => (
-          props.match
-            ? <PostSingle {...props}/> : ''
-        )}/>
+  return (
+    <div className="c-post">
+      <div className="c-post__title-box">
+        <Link to={`/post/${id}`} className="c-post__title">{title}</Link>
       </div>
-    );
-  }
+      <div className="c-post__info">{info}</div>
+      <div className="c-post__tags">
+        <Tags tags={tags} />
+      </div>
+      <Route path="/post/:postId"  children={(props) => (
+        props.match
+          ? <PostSingle {...props}/> : ''
+      )}/>
+    </div>
+  );
 }
 
 export default Post;
